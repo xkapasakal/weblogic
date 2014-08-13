e3d967d66b45554ffca951c0526ae403197b7e8b
Christos Kapasakalidis <xkapasakal@gmail.com>
2014-08-13 12:33:14 +0300

    Update recipe to make it more generic

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,42 +2,57 @@
 # Cookbook Name:: weblogic
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Copyright (C) 2014 Christos Kapasakalidis
 #
-# All rights reserved - Do Not Redistribute
+# All rights reserved
 #
 
-# Install Java
-include_recipe 'java'
+unless ::Dir.exists?("/home/vagrant/bea")
+  # Install Java
+  include_recipe 'java'
 
-remote_file "/home/vagrant/V13672-01.zip" do
-  source node['weblogic']['remote_installation_file']
-  owner "vagrant"
-  group "vagrant"
-  checksum "d811df7ae7852f70f59bb2ef8bf313358918bcaf33dc824e67471eb849a866ba"
-  action :create
+  remote_file "/home/vagrant/weblogic-installer.zip" do
+    source node['weblogic']['remote_installation_file']
+    owner "vagrant"
+    group "vagrant"
+    checksum "d811df7ae7852f70f59bb2ef8bf313358918bcaf33dc824e67471eb849a866ba"
+    action :create
+  end
+
+  # Install unzip
+  yum_package "unzip" do
+    action :install
+  end
+
+  execute "unzip weblogic zip" do
+    cwd "/home/vagrant"
+    user "vagrant"
+    command "unzip -o /home/vagrant/weblogic-installer.zip -d /home/vagrant/weblogic-installer"
+    not_if { ::Dir.exists?("/home/vagrant/weblogic-installer")}
+  end
+
+  template "/home/vagrant/silent.xml" do
+    source "silent.xml.erb"
+    mode 0660
+    owner "vagrant"
+    group "vagrant"
+  end
+
+  execute "install weblogic" do
+    cwd "/home/vagrant/weblogic-installer"
+    user "vagrant"
+    command "java -jar server103_generic.jar -mode=silent -silent_xml=/home/vagrant/silent.xml"
+    not_if { ::Dir.exists?("/home/vagrant/bea")}
+  end
+
+  # Delete unneeded file and directory
+  directory "/home/vagrant/weblogic-installer" do
+    recursive true
+    action :delete
+  end
+
+  file "/home/vagrant/weblogic-installer.zip" do
+    action :delete
+  end
+
 end
-
-yum_package "unzip" do
-  action :install
-end
-
-execute "unzip weblogic zip" do
-  cwd "/home/vagrant"
-  user "vagrant"
-  command "unzip -o /home/vagrant/V13672-01.zip -d /home/vagrant/V13672-01"
-end
-
-template "/home/vagrant/silent.xml" do
-  source "silent.xml.erb"
-  mode 0660
-  owner "vagrant"
-  group "vagrant"
-end
-
-execute "install weblogic" do
-  cwd "/home/vagrant/V13672-01"
-  user "vagrant"
-  command "java -jar server103_generic.jar -mode=silent -silent_xml=/home/vagrant/silent.xml"
-end
-
